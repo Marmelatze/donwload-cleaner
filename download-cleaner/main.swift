f27b7dc5e472e46633e19f53098febc8d4f3d33c
Florian Pfitzer <florian@pfitzer.me>
2020-03-14 16:20:55 +0100

    Initial Commit

---
+++ download-cleaner/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  download-cleaner
+//
+//  Created by Florian Pfitzer on 14.03.20.
+//  Copyright © 2020 Florian Pfitzer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
