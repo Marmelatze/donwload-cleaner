b361063b2701293c833148ae99e7bc8f4cce6a88
Florian Pfitzer <florian@pfitzer.me>
2020-03-15 11:03:50 +0100

    remove debug outputt

--- download-cleaner/main.swift
+++ download-cleaner/main.swift
@@ -14,12 +14,10 @@ let documentsUrl =  FileManager.default.urls(for: .downloadsDirectory, in: .user
 do {
     // Get the directory contents urls (including subfolders urls)
     let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil)
-    print(directoryContents)
     let currentDate = Date()
     var diff = DateComponents()
     diff.day = -7
     let deletePastDate = Calendar.current.date(byAdding: diff, to: currentDate)
-    print(deletePastDate)
     for url in directoryContents {
         if (url.lastPathComponent.starts(with: ".")) {
             continue
