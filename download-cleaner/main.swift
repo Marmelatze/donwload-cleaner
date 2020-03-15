3b22c24572fb4ab07b1ea25a4d73cd3bfc921b21
Florian Pfitzer <florian@pfitzer.me>
2020-03-15 10:59:17 +0100

    init

--- download-cleaner/main.swift
+++ download-cleaner/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+// Get the document directory url
+let documentsUrl =  FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!
 
+do {
+    // Get the directory contents urls (including subfolders urls)
+    let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil)
+    print(directoryContents)
+    let currentDate = Date()
+    var diff = DateComponents()
+    diff.day = -7
+    let deletePastDate = Calendar.current.date(byAdding: diff, to: currentDate)
+    print(deletePastDate)
+    for url in directoryContents {
+        if (url.lastPathComponent.starts(with: ".")) {
+            continue
+        }
+        let attr = try FileManager.default.attributesOfItem(atPath: url.path)
+        let modificationDate = attr[FileAttributeKey.modificationDate] as? Date
+        if (modificationDate ?? currentDate < deletePastDate!) {
+            print("removing " + url.path)
+            
+            try FileManager.default.removeItem(at: url)
+        }
+    }
+
+
+} catch {
+    print(error)
+}
